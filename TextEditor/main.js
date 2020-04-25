eaeb2484bb5d16e2b6c0aa171432294dde4c73e0
ayrtton <46231350+ayrtton@users.noreply.github.com>
2020-04-25 15:38:10 -0300

    Add files via upload

--- TextEditor/main.js
+++ TextEditor/main.js
@@ -2,7 +2,6 @@ function changeStyle(id) {
     var sel = window.getSelection(); 
 
     if (sel.rangeCount) {
-      
         var e = document.createElement('span');
         
         switch(id) {
@@ -22,29 +21,67 @@ function changeStyle(id) {
                 e.style.fontFamily = "Lucida Console";
                 break;
 
+            case '4':
+                e.innerHTML = "• ";
+
+                break;
+
             case '5':
                 e.style.textDecoration = "underline";
                 break;
 
             case '6':
-                e.style.backgroundColor = "LightSteelBlue";
+                var d = document.createElement('div');
+                var e2 = document.createElement('span');
+                var size = sel.toString().length * 12 + 20;
+                
+                e2.style.fontSize = "xx-large";
+                e2.innerHTML = "\" ";
+                e.appendChild(e2);
+                
+                d.style.backgroundColor = "LightSteelBlue";
+                d.style.width = size +"px";
+                d.appendChild(e);
+                e = d;
+
                 break;
 
             case '7':
-                window.prompt("Enter the URL of the link: ");
+                var url = window.prompt("Enter the URL of the link: ");
+  
                 e.style.color = "blue";
                 e.style.textDecoration = "underline";
+                e.style.cursor = "pointer";
+                e.addEventListener("click", function(){
+                    window.location.replace(url);
+                });
+
                 break;
 
             default:
                 break;
         }    
 
-        e.innerHTML = sel.toString(); 
+        e.innerHTML += sel.toString(); 
 
         var range = sel.getRangeAt(0);
         range.deleteContents(); 
         range.insertNode(e); 
     }
-
 }
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+
+    if (keyName === 'Alt') {
+        return;
+    }
+  
+    if (event.altKey) {
+        changeStyle(keyName);
+    } 
+}, false);
+
+document.addEventListener('click', (event) => {
+    changeStyle(window.event.target.getAttribute('id'));
+}, false);
